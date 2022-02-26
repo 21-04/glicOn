cab4f80b81de06a8710e596ebd8ab5a4bc909b34
NOSI / GIS  - Claudina Sofia da V. Moreira <claudina.moreira@nosi.cv>
2022-02-26 08:32:05 -0100

    added update

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # main.py
 from fastapi import FastAPI
+from os import stat
+from turtle import st
+from fastapi import FastAPI,status,HTTPException
+from pydantic import BaseModel
+from typing import Optional,List
+from database import SessionLocal
+import models
 app = FastAPI()
 
 @app.get("/")
