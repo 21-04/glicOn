85604918381e50c5c7c81b3d4789df146e5d1665
NOSI / GIS  - Claudina Sofia da V. Moreira <claudina.moreira@nosi.cv>
2022-02-27 11:10:21 -0100

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #from typing import List
 from fastapi import Depends, FastAPI, HTTPException, status,File, UploadFile
-#from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session
 #from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 #import models, schemas
 #from .database import SessionLocal, engine
