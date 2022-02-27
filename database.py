ef5456f94a9b32030260a5fef6a383967652c516
NOSI / GIS  - Claudina Sofia da V. Moreira <claudina.moreira@nosi.cv>
2022-02-27 13:16:40 -0100

    update

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import create_engine
+#from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+import os
+from sqlalchemy.dialects.postgresql import *
 #engine=create_engine("postgresql://postgres:postgres@localhost/glic_on",
-engine=create_engine("postgres://ckqgsphkotwums:23579a1afa2016c0b6fde9a01e0d0d1567183513ef402b8894d08bf7753e0f9d@ec2-3-209-226-234.compute-1.amazonaws.com:5432/d4tqr0kj7tedd9",
+url="postgres://vpfamjobcgsdey:42d7379e819411a64c12846e52b2ff2d6f672d4c1d67e737ac9b3febb286be46@ec2-3-227-195-74.compute-1.amazonaws.com:5432/d9b0m9c48qafim"
+engine=create_engine(url,
     echo=True
 )
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import List
 from fastapi import Depends, FastAPI, HTTPException, status,File, UploadFile
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-#import models, schemas
+import models
 #from database import SessionLocal
 from datetime import datetime, timedelta
 import shutil
